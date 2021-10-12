c0ea39c6262ce3d7fc51468f9265e428122c0f36
zxcvbinz <davide.lanotte.unix@gmail.com>
2021-10-12 15:51:37 +0200

    changed workspace

--- install_custom.sh
+++
@@ -1,6 +0,0 @@
-cp -f .zshrc ~/ 
-echo [zshrc Moved ✅]
-cp -f ./.Sublivim/vimrc ~/.Sublivim/
-echo [vimrc Moved ✅]
-cp -f .Sublivim/vim/colors/orbital.vim ~/.Sublivim/vim/colors/
-echo [orbital Moved ✅]
